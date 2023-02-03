0d070611c8308dd719175ff106d54ebe91f2faee
DerzeTTTT <u13_davdro_lbn@technischools.com>
2023-02-03 11:26:21 +0100

    added branch and main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
-print("OKEY")
+import math;
+import time;
+
+import mouse;
+
+for i in range(360):
+    
+    angle = i/200;
+    X,Z = math.sin(angle)*10, math.cos(angle)*10;
+
+    mouse.move(X,Z, absolute=True);
+
+    time.sleep(.05);
